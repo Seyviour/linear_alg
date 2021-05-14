3bb94c12d12b8fc5ca158f6110b2ddf24dd3e3f6
Saviour <owolabisaviour@gmail.com>
2021-05-14 07:42:20 +0100

    Feat: First working version

--- gauss_linear.py
+++ gauss_linear.py
@@ -40,7 +40,7 @@ def sub(x1:list, x2:list):
         raise Exception()
     return [x1 - x2 for x1, x2 in zip(x1, x2)]
 
-def mult(x1:list, const_val: Fraction)
+def mult(x1:list, const_val: Fraction):
     return [x * const_val for x in x1]
 
 def div (x1:list, const_val: Fraction): 
@@ -53,15 +53,45 @@ def swap(equation: list, index1: int, index2: int):
     equation[index1], equation[index2] = equation[index2], equation[index1]
 
 
-def find_first_non_zero(equation:list, var:int, start_index:int)
-    for eq, idx in enumerate(equation[start_index:], start=start_index)
+def find_first_non_zero(equation:list, var:int, start_index:int): 
+    for idx, eq in enumerate(equation[start_index:], start=start_index):
         if (eq[var] != 0): 
             return idx
     
     return -1 #dangerous since -1 is a valid index
-    
+
+def product_sum(equation:list, start_idx: int):  # really need to rework these variable names :#
+    for idx in range(start_idx+1, len(equation)): 
+        r = equation[idx][start_idx]/equation[start_idx][start_idx]
+        equation[idx] = sub(equation[idx],  mult(equation[start_idx], r))
+
+def pretty_output(equation): 
+    for eq in equation:
+        print(eq)
+        
+
 def gaussian_reduce(equation: list):
-     
+    #Limit to n equations in n variables for now
+    #Could possibly be more elegant with recursion. Iteration go brrr tho
+
+    for idx, eq in enumerate(equation):
+
+        if (idx == len(equation)-1): 
+            pretty_output(equation)
+            break 
+
+        first_non_zero = find_first_non_zero(equation, idx, idx)
+        if (first_non_zero == -1): 
+            print("Evaluation Error: Something something matrix property")
+        else: 
+            swap(equation, idx, first_non_zero)
+            product_sum(equation, idx)
+
+
+if __name__ == "__main__": 
+    gaussian_reduce([[1,2,3], [3,4,5]])
+
+
 
 
 
