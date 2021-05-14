2db572ada0bce8d171a507bc4b4a80f623228af6
Saviour <owolabisaviour@gmail.com>
2021-05-14 03:12:11 +0100

    Initial Commit: Gausian elimination

---
+++ gauss_linear.py
@@ -0,0 +1,12 @@
+# Input is a 2d array
+# Output should be a 2d array in echelon 
+
+# Input Structure: Final element on each row of 2-d array should be y
+# all preceding elements shoud be xs
+# Input elements must be integers biko
+# To think of it, that shouldn't be necessary since na computer go dey do the multiplication and division
+# computers don't handle fractions well sha
+
+import numpy as np 
+from fractions import Fraction
+
